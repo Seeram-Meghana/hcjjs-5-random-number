9a5fbc156559d0756d886b436eb8156df695e98c
Seeram-Meghana <seermeghan7@gmail.com>
2024-07-05 17:19:32 +0530

    check in

--- src/script.js
+++ src/script.js
@@ -1,11 +1,10 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const number = document.querySelector(".number");
     const generateBtn = document.querySelector(".generate");
     const resetBtn = document.querySelector(".reset");
 
     const generateNumber = () => {
-        const randomNumber = Math.floor(Math.random() * 1001); // Generate number between 0 and 1000
+        const randomNumber = Math.floor(Math.random() * 1001);
         number.textContent = randomNumber;
     };
 
